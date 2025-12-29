5325fa33f0eec73f48dbf911f73513a2e1a7f3f0
BesBobowyy <banan4ik.stop.channel@gmail.com>
2025-12-29 17:48:17 +0300

    Update the decorators arguments with constants, removed trash comments.

--- core/utils/decorators.py
+++ core/utils/decorators.py
@@ -1,31 +1,4 @@
-'''
-import sys
-
-if sys.version_info >= (3, 13):
-    # Python 3.13+
-    from warnings import deprecated as builtin_deprecated
-
-    def deprecated(version: str = '<unknown>', reason: str = "Deprecated Method") -> Callable[..., Any]:
-        decorator = builtin_deprecated
-        decorator.message = f"{reason} [Version: {version}]"    # pyright: ignore[reportAttributeAccessIssue]
-        return decorator
-else:
-    # Python -3.12.X
-    from functools import wraps
-    import warnings
-
-    def deprecated(version: str = '<unknown>', reason: str = "Deprecated Method") -> Callable[..., Any]:
-        def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
-            @wraps(func)
-            def wrapper(*args: Any, **kwargs: Any) -> Any:
-                warnings.warn(
-                    f"{func.__name__}: {reason} [Version: {version}]",
-                    DeprecationWarning,
-                    stacklevel=2
-                )
-                return func(*args, **kwargs)
-            return wrapper
-        return decorator'''
+from core.utils.constants import Strings
 
 from functools import wraps
 
@@ -33,7 +6,7 @@ from typing import Callable, Any
 
 import warnings
 
-def deprecated(version: str = '<unknown>', reason: str = "Deprecated Method") -> Callable[..., Any]:
+def deprecated(version: str = Strings.UNKNOWN_VERSION, reason: str = Strings.DEPRECATED_METHOD) -> Callable[..., Any]:
     def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
         @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
@@ -46,7 +19,7 @@ def deprecated(version: str = '<unknown>', reason: str = "Deprecated Method") ->
         return wrapper
     return decorator
 
-def alternative(version: str = '<unknown>', reason: str = "Experimental Feature") -> Callable[..., Any]:
+def alternative(version: str = Strings.UNKNOWN_VERSION, reason: str = Strings.ALTERNATIVE_METHOD) -> Callable[..., Any]:
     def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
         @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
