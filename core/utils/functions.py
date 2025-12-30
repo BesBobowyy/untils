44c7c673ee960c37eab257c675897eb4a1f1105b
BesBobowyy <banan4ik.stop.channel@gmail.com>
2025-12-30 18:19:28 +0300

    Update `warning` function with extended controls.

--- core/utils/functions.py
+++ core/utils/functions.py
@@ -2,21 +2,25 @@ from core.utils.enums import WarningsLevel
 
 from core.settings import Settings
 
+from typing import Type, Optional, Tuple
+
 import warnings
 
 def warning(
     settings: Settings,
     message: str,
     auto_correct: str,
-    warning: type[Warning],
-    exception: type[Exception]
+    warning: Type[Warning]=Warning,
+    exception: Type[Exception]=Exception,
+    warning_levels: Optional[Tuple[WarningsLevel]]=None,
+    exception_levels: Optional[Tuple[WarningsLevel]]=None
 ) -> None:
     """Warning or exception in validators."""
-    if settings.warnings_level == WarningsLevel.Basic:
+    if settings.warnings_level in (warning_levels or ()):
         warnings.warn(
             message + auto_correct,
             warning,
             stacklevel=2
         )
-    elif settings.warnings_level == WarningsLevel.Strict:
+    elif settings.warnings_level in (exception_levels or ()):
         raise exception(message)
