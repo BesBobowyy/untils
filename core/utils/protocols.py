1d00edba147c1126fdd7575f28f94a9ba806e553
BesBobowyy <banan4ik.stop.channel@gmail.com>
2025-12-31 02:54:24 +0300

    Add protocols.

---
+++ core/utils/protocols.py
@@ -0,0 +1,18 @@
+from core.utils.type_aliases import UnknownConfigType
+
+from core.settings import Settings
+
+from typing import Protocol, Any, runtime_checkable
+
+@runtime_checkable
+class IOReaderMixin(Protocol):
+    """Protocol for all IOReader mixins."""
+
+    @staticmethod
+    def read(file_path: str) -> UnknownConfigType: ...
+
+class Factoric(Protocol):
+    """Factory protocol."""
+
+    @classmethod
+    def create(cls, settings: Settings, *args: Any, **kwargs: Any) -> Any: ...
