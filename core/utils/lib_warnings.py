fcab8e3d610063877e55b06a710cb1dcb9c2294c
BesBobowyy <banan4ik.stop.channel@gmail.com>
2025-12-31 02:54:55 +0300

    Add custom warnings.

---
+++ core/utils/lib_warnings.py
@@ -0,0 +1,8 @@
+class FileWarning(Warning):
+    """Warning class for IOReader issues."""
+
+class ConfigStructureWarning(Warning):
+    """Warning class for config structure issues."""
+
+class ConfigValuesWarning(Warning):
+    """Warning class for config values issues."""
