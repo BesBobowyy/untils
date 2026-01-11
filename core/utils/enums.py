9382a6b150aa1d25d688830c9c35ca66690a4949
BesBobowyy <banan4ik.stop.channel@gmail.com>
2026-01-11 20:34:21 +0300

    Update comments and docstrings.

--- core/utils/enums.py
+++ core/utils/enums.py
@@ -13,28 +13,22 @@ class LibraryVersionStatus(IntEnum):
     """Release."""
 
 class WarningsLevel(IntEnum):
-    """Controls, how much errors affect on code flow."""
+    """Error alerts level."""
 
-    '''
-    IgnoreErrors = 2**0    # No errors, auto-correcting.
-    IgnoreWarnings = 2**1    # No warnings, auto-correcting.
-    BasicErrors = 2**2    # Throws critical errros, but immaterial errors ignores.
-    BasicWarnings = 2**3     # Displays critical warnings, but immaterial warnings ignores.
-    StrictErrors = 2**4    # Throws any errors.
-    StrictWarnings = 2**5    # Display all warnings.
-    '''
-
-    Ignore = 0    # No errors, auto-correcting.
-    Basic = 1    # Critical errors.
-    Strict = 2    # Any errors and warnings.
+    Ignore = 0
+    """Silent errors."""
+    Basic = 1
+    """Display with auto-correcting."""
+    Strict = 2
+    """Raise exceptions."""
 
 class ConfigVersions(IntEnum):
-    """All config versions."""
+    """The all config versions."""
 
     V1 = 1
 
 class RawTokenType(IntEnum):
-    """Raw token type."""
+    """The raw token type."""
 
     SPACE = 0
     WORD = 1
@@ -46,7 +40,7 @@ class RawTokenType(IntEnum):
         return self.name
 
 class FinalTokenType(IntEnum):
-    """Final token type."""
+    """The final token type."""
 
     WORD = 0
     FLAG = 1
@@ -56,7 +50,9 @@ class FinalTokenType(IntEnum):
         return self.name
 
 class InternalState(Enum):
-    """All internal states in config."""
+    """The all internal states in config."""
 
     BASE = "__base__"
+    """Commands in this state will be always available."""
     INIT = "__init__"
+    """Initial state."""
