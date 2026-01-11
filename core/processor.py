bcb22541fd6a490c34940ac98b7f38e741a51b25
BesBobowyy <banan4ik.stop.channel@gmail.com>
2026-01-11 19:01:57 +0300

    Add `process_input` for input processing.

--- core/processor.py
+++ core/processor.py
@@ -1,10 +1,16 @@
-from core.utils.type_aliases import UnknownConfigType, ConfigType
+from core.utils.type_aliases import UnknownConfigType, ConfigType, InputDict
+from core.utils.protocols import FinalInputProtocol
 
 from core.ioreader import IOReader
 from core.config_validator import ConfigValidator
 from core.parser import Parser
 from core.commands_config import CommandsConfig
 from core.settings import Settings
+from core.input_token import RawInputToken
+from core.tokenizer import Tokenizer
+from core.input_validator import InputValidator
+
+from typing import List
 
 class Processor:
     """Class for config processing."""
@@ -29,6 +35,29 @@ class Processor:
         if debug: print("Parsed config: <TODO>")
 
         return CONFIG
+    
+    @staticmethod
+    def process_input(settings: Settings, input_str: str, debug: bool=False) -> InputDict:
+        if debug: print(f"--- Processing input string: '{input_str}' ---")
+
+        ### 1. Tokenizer ###
+        if debug: print("--- Tokenizing the input ---")
+        tokenizer: Tokenizer = Tokenizer(input_str, debug)
+        TOKENS: List[RawInputToken] = tokenizer.tokenize_input()
+        if debug: print(f"Tokens: {TOKENS}")
+
+        ### 2. InputValidator ###
+        if debug: print("--- Validating the input ---")
+        input_validator: InputValidator = InputValidator(TOKENS, debug)
+        VALIDATED_TOKENS: List[FinalInputProtocol] = input_validator.validate_input(settings)
+        if debug: print(f"Validated tokens: {VALIDATED_TOKENS}")
+
+        ### 3. Parser ###
+        if debug: print("--- Parsing the input ---")
+        PARSED_REPRESENTATION: InputDict = Parser.parse_input(VALIDATED_TOKENS, debug)
+        if debug: print(f"Parsed input: {PARSED_REPRESENTATION}")
+
+        return PARSED_REPRESENTATION
 
 
 
