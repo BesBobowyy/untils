108f5bcca272c1c290f0ba2a7baf8c6f486b2903
BesBobowyy <banan4ik.stop.channel@gmail.com>
2026-01-11 18:56:30 +0300

    Fixed initialize bug in `__init__` method, enhanced `current_state` property for several types.

--- core/settings.py
+++ core/settings.py
@@ -1,13 +1,16 @@
-from core.utils.enums import WarningsLevel
+from core.utils.enums import WarningsLevel, InternalState
 from core.utils.decorators import alternative
 from core.utils.constants import Strings
+
+from typing import Union
 
 class Settings:
     """ContextSettings."""
 
-    __slots__ = ["__warnings_level"]
+    __slots__ = ["__warnings_level", "__current_state"]
 
     __warnings_level: WarningsLevel
+    __current_state: str
 
     @property
     def warnings_level(self) -> WarningsLevel:
@@ -17,9 +20,22 @@ class Settings:
     @warnings_level.setter
     def warnings_level(self, value: WarningsLevel) -> None:
         self.__warnings_level = value
+    
+    @property
+    def current_state(self) -> str:
+        """Current commands context state."""
+        return self.__current_state
+
+    @current_state.setter
+    def current_state(self, value: Union[str, InternalState]) -> None:
+        if type(value) == str:
+            self.__current_state = value
+        elif type(value) == InternalState:
+            self.__current_state = value.value
 
     def __init__(self) -> None:
         self.__warnings_level = WarningsLevel.Strict
+        self.current_state = InternalState.INIT.value
 
     @alternative(version=Strings.ANY_VERSION)
     def get_warnings_level(self) -> WarningsLevel:
