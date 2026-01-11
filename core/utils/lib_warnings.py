6c8c39386560566600ab265c2424f54a498db63d
BesBobowyy <banan4ik.stop.channel@gmail.com>
2026-01-11 22:09:15 +0300

    Add base classes for each section (Config, Input) and add exception classes.

--- core/utils/lib_warnings.py
+++ core/utils/lib_warnings.py
@@ -1,14 +1,41 @@
-class FileWarning(Warning):
+class ConfigWarning(Warning):
+    """Warning class for all config validation issues."""
+
+class FileWarning(ConfigWarning):
     """Warning class for IOReader issues."""
 
-class ConfigStructureWarning(Warning):
+class ConfigStructureWarning(ConfigWarning):
     """Warning class for config structure issues."""
 
-class ConfigValuesWarning(Warning):
+class ConfigValuesWarning(ConfigWarning):
     """Warning class for config values issues."""
 
-class InputStructureWarning(Warning):
+class InputWarning(Warning):
+    """Warning class for all input validation issues."""
+
+class InputStructureWarning(InputWarning):
     """Warning class for input structure issues."""
 
-class InputValuesWarning(Warning):
+class InputValuesWarning(InputWarning):
     """Warning class for input values issues."""
+
+class ConfigError(Exception):
+    """Exception class for all config validation issues."""
+
+class FileError(ConfigError):
+    """Exception class for IOReader issues."""
+
+class ConfigStructureError(ConfigError):
+    """Config exception class for config structure issues."""
+
+class ConfigValuesError(ConfigError):
+    """Config exception class for config values issues."""
+
+class InputError(Exception):
+    """Exception class for all input validation issues."""
+
+class InputStructureError(Exception):
+    """Input exception class for input structure issues."""
+
+class InputValuesError(Exception):
+    """Input exception class for input values issues."""
