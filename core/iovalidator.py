ce4e6bae1abbaac14bfc1720ac57fdc387826e40
BesBobowyy <banan4ik.stop.channel@gmail.com>
2026-01-13 22:17:53 +0300

    Update comments and docstrings.

--- core/iovalidator.py
+++ core/iovalidator.py
@@ -8,14 +8,31 @@ from core.settings import Settings
 import os
 
 class IOValidator:
-    """Validator class for IO operations."""
+    """Validator class for the all IO operations for `IOReader`."""
 
     @staticmethod
     def validate_config_path(settings: Settings, file_path: str) -> bool:
+        """Validates a config path.
+        
+        Args:
+            settings: The settings.
+            file_path: The file path.
+
+        Returns:
+            `True` if the config path is valid and supported, else `False`.
+
+        Raises:
+            FileWarning: The config file is not exists or the config extension is not in standart.
+
+            FileError: The config file is not exists.
+        """
+
         if os.path.exists(file_path) and os.path.isfile(file_path):
+            # Processing the config file path.
             extension: str = os.path.splitext(file_path)[1]
             if extension in Constants.SUPPORTED_CONFIG_FORMATS:
                 if extension not in Constants.STANDART_CONFIG_FORMATS:
+                    # The config format is not in standart.
                     warning(
                         settings,
                         Strings.CONFIG_EXTENSION_NOT_SUPPORTS,
