57de0192dd268fabc6d5642225245c98f82e0476
BesBobowyy <banan4ik.stop.channel@gmail.com>
2026-01-13 22:10:23 +0300

    Separate `IOReaderMixin` to `IOReaderMixin` as abstract class and `IOReaderProtocol` as `IOReaderMixin` protocol. Update comments and docstrings.

--- core/utils/protocols.py
+++ core/utils/protocols.py
@@ -4,22 +4,31 @@ from core.utils.enums import FinalTokenType
 from core.settings import Settings
 
 from typing import Protocol, Any, runtime_checkable
+from abc import ABC, abstractmethod
+
+class IOReaderMixin(ABC):
+    """The abstract class for all IOReader mixins."""
+
+    @staticmethod
+    @abstractmethod
+    def read(settings: Settings, file_path: str) -> UnknownConfigType:
+        """Reads a config file."""
 
 @runtime_checkable
-class IOReaderMixin(Protocol):
-    """Protocol for all IOReader mixins."""
+class IOReaderProtocol(Protocol):
+    """The protocol for all IOReader mixins."""
 
     @staticmethod
     def read(settings: Settings, file_path: str) -> UnknownConfigType: ...
 
 class Factoric(Protocol):
-    """Factory protocol."""
+    """The protocol for all factories."""
 
     @classmethod
     def create(cls, settings: Settings, *args: Any, **kwargs: Any) -> Any: ...
 
 class FinalInputProtocol(Protocol):
-    """Final input token protocol."""
+    """The protocol for all final input tokens."""
 
     type: FinalTokenType
     value: Any
