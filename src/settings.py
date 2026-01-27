42642ed6a5015277df523773c4b02cc73affab08
BesBobowyy <banan4ik.stop.channel@gmail.com>
2026-01-27 12:16:18 +0300

    Refactor code as linter rules.

--- src/settings.py
+++ src/settings.py
@@ -1,10 +1,14 @@
-from src.utils.enums import WarningsLevel, InternalState
-from src.utils.decorators import alternative
-from src.utils.constants import Strings
+"""settings.py - Global context settings for much part of all library."""
+
+# pyright: reportUnnecessaryIsInstance=false
 
 from typing import Union
 
 import logging
+
+from src.utils.enums import WarningsLevel, InternalState
+from src.utils.decorators import alternative
+from src.utils.constants import Strings
 
 class Settings:
     """The global context settings."""
@@ -23,7 +27,7 @@ class Settings:
     @warnings_level.setter
     def warnings_level(self, value: WarningsLevel) -> None:
         self.__warnings_level = value
-    
+
     @property
     def current_state(self) -> str:
         """Current commands state."""
@@ -31,18 +35,18 @@ class Settings:
 
     @current_state.setter
     def current_state(self, value: Union[str, InternalState]) -> None:
-        if type(value) == str:
+        if isinstance(value, str):
             self.__current_state = value
-        elif type(value) == InternalState:
+        elif isinstance(value, InternalState):
             self.__current_state = value.value
-    
+
     @property
     def logger(self) -> logging.Logger:
         """Returns settings logger."""
         return self.__logger
 
     def __init__(self) -> None:
-        self.__warnings_level = WarningsLevel.Strict
+        self.__warnings_level = WarningsLevel.STRICT
         self.__current_state = InternalState.INIT.value
         self.__logger = logging.getLogger(__name__)
         self.__logger.debug(Strings.LOG_SETTINGS_INIT)
@@ -57,9 +61,9 @@ class Settings:
         """
 
         return self.__warnings_level
-    
+
     @alternative(version=Strings.ANY_VERSION)
-    def set_warnings_level(self, warnings_level: WarningsLevel=WarningsLevel.Strict) -> None:
+    def set_warnings_level(self, warnings_level: WarningsLevel=WarningsLevel.STRICT) -> None:
         """[!ALT] Sets a warnings level.\n
         <This method is alternative for the standart. In real product use the property `Settings.warnings_level`.>
         
