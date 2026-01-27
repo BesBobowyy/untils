1acff171da052e02760da2ca0c1af0205501030f
BesBobowyy <banan4ik.stop.channel@gmail.com>
2026-01-27 12:19:01 +0300

    Refactor code as linter rules.

--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,21 @@
-"""`mplib` is a light-weight library for console games or non-system utils. You can validate the raw input string with JSON-config files."""
+"""*mplib* - Is light-weight library for console games or small utils to process user input as commands and describe structure with config."""
 
 # pyright: reportUnusedImport=false
 # ^^^^^^^ (Public imports.)
 
-import src.utils as utils
+from src import utils
 
 from src.command_system import CommandSystem
-from src.command import AliasNode, CommandNode, CommandWordNode, CommandFallbackNode, CommandFlagNode, CommandOptionNode, StateNode
+from src.command import (
+    AliasNode, CommandNode, CommandWordNode, CommandFallbackNode, CommandFlagNode,
+    CommandOptionNode, StateNode
+)
 from src.commands_config import CommandsConfig
 from src.config_validator import ConfigValidator
 from src.factories import CommandNodeFactory
-from src.input_token import RawInputToken, FinalInputTokenWord, FinalInputTokenFlag, FinalInputTokenOption
+from src.input_token import (
+    RawInputToken, FinalInputTokenWord, FinalInputTokenFlag, FinalInputTokenOption
+)
 from src.input_validator import InputValidator, ParsedInputValidator
 from src.ioreader import IOReader
 from src.iovalidator import IOValidator
