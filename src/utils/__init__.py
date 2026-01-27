10b707938e4ade860baa9a4d225d3f6c51a1e6ef
BesBobowyy <banan4ik.stop.channel@gmail.com>
2026-01-27 12:18:56 +0300

    Refactor code as linter rules.

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -4,17 +4,20 @@
 # ^^^^^^^ (Public imports.)
 
 from src.utils.lib_warnings import (
-    ConfigWarning, FileWarning, ConfigStructureWarning, ConfigValuesWarning, InputWarning, InputStructureWarning,
-    InputValuesWarning, ConfigError, FileError, ConfigStructureError, ConfigValuesError, InputError, InputStructureError,
-    InputValuesError
+    ConfigWarning, FileWarning, ConfigStructureWarning, ConfigValuesWarning, InputWarning,
+    InputStructureWarning,InputValuesWarning, ConfigError, FileError, ConfigStructureError,
+    ConfigValuesError, InputError, InputStructureError, InputValuesError
 )
 from src.utils.type_aliases import (
-    ConfigVersion, CommandClass, UnknownCommandClass, CommandType, InternalCommandStates, CommandStates,
-    ConfigSupportedExtensions, CommandConfig_Unknown, CommandConfig_Word, CommandConfig_Fallback, CommandConfig_Flag,
-    CommandConfig_Option, ConfigType, UnknownConfigType, InputDict, CommandPath
+    ConfigVersion, CommandClass, UnknownCommandClass, CommandType,
+    InternalCommandStates, CommandStates, ConfigSupportedExtensions, UnknownCommandConfig,
+    WordCommandConfig, FallbackCommandConfig, FlagCommandConfig, OptionCommandConfig,
+    ConfigType, UnknownConfigType, InputDict, CommandPath
 )
 from src.utils.constants import Constants, Strings
 from src.utils.decorators import deprecated, alternative
-from src.utils.enums import WarningsLevel, ConfigVersions, RawTokenType, FinalTokenType, InternalState
+from src.utils.enums import (
+    WarningsLevel, ConfigVersions, RawTokenType, FinalTokenType, InternalState
+)
 from src.utils.functions import warning
 from src.utils.protocols import IOReaderMixin, Factoric, FinalInputProtocol
