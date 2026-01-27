1532293486b4439e84de00a489d35a040c051f94
BesBobowyy <banan4ik.stop.channel@gmail.com>
2026-01-27 12:09:03 +0300

    Refactor code as linter rules. Change docstrings.

--- src/command_system.py
+++ src/command_system.py
@@ -1,3 +1,9 @@
+"""command_system.py - Command system."""
+
+# pyright: reportUnnecessaryIsInstance=false
+
+from typing import Optional, List, Union, cast, Dict, Tuple
+
 from src.utils.type_aliases import InputDict, CommandPath, CallableCommand, CommandHistory
 from src.utils.constants import Strings
 
@@ -6,8 +12,6 @@ from src.settings import Settings
 from src.processor import Processor
 from src.input_validator import ParsedInputValidator
 from src.command import CommandNode, CommandWordNode, CommandFallbackNode
-
-from typing import Optional, List, Union, cast, Dict, Tuple
 
 class CommandSystem:
     """Core class with command config, API, processing and much more."""
@@ -40,12 +44,12 @@ class CommandSystem:
             "is_write_overflow": True,
             "notes": []
         } if history is None else history
-    
+
     def is_config_loaded(self) -> bool:
         """Returns a `bool` value, what determines is config loaded."""
 
         return self.config is not None
-    
+
     def load_config(self, config_path: str) -> None:
         """Loads a commands config.
         
@@ -54,7 +58,7 @@ class CommandSystem:
         """
 
         self.config = Processor.load_config(self.settings, config_path)
-    
+
     def set_config(self, config: Optional[CommandsConfig]) -> None:
         """Sets already cached config or deletes exist.
         
@@ -63,7 +67,7 @@ class CommandSystem:
         """
 
         self.config = config
-    
+
     def process_input(self, input_str: str) -> InputDict:
         """Processes a user input and returns `InputDict` as input representation.
         
@@ -75,7 +79,7 @@ class CommandSystem:
         """
 
         return Processor.process_input(self.settings, self.config, input_str)
-    
+
     def is_input_valid(self, input_dict: InputDict) -> bool:
         """Validates an input.
         
@@ -88,11 +92,11 @@ class CommandSystem:
 
         if self.config is not None:
             return ParsedInputValidator.validate_input_dict(self.settings, input_dict, self.config)
-        
+
         self.settings.logger.warning(Strings.LOG_CONFIG_NOT_LOADED)
 
         return False
-    
+
     def get_normalized_path(self, input_dict: InputDict) -> List[str]:
         """Returns original key-names in config by `path` in `input_dict`.
         
@@ -117,7 +121,10 @@ class CommandSystem:
             for command in commands:
                 if command.type == "word":
                     command = cast(CommandWordNode, command)
-                    if part == command.name or part in [alias.alias_name for alias in command.aliases]:
+                    if (
+                        part == command.name
+                        or part in [alias.alias_name for alias in command.aliases]
+                    ):
                         result.append(command.name)
                         commands = command.children
                         break
@@ -126,11 +133,11 @@ class CommandSystem:
                     result.append(command.name)
                     commands = command.children
                     break
-        
+
         self.settings.logger.info(Strings.LOG_CALCULATE_NORMALIZED_PATH_END)
-        
+
         return result
-    
+
     def get_all_commands(self) -> List[CommandNode]:
         """Returns all commands as command nodes."""
 
@@ -143,9 +150,9 @@ class CommandSystem:
         for command in self.config.commands:
             if command.type in ("word", "fallback"):
                 result.append(command)
-        
+
         return result
-    
+
     def get_all_commands_str(self) -> List[str]:
         """Returns all commands as name strings."""
 
@@ -158,7 +165,7 @@ class CommandSystem:
         for command in self.config.commands:
             if command.type in ("word", "fallback"):
                 result.append(command.name)
-        
+
         return result
 
     def get_available_commands(self) -> List[CommandNode]:
@@ -175,7 +182,7 @@ class CommandSystem:
                 for state in self.config.states:
                     if command.name in state.commands:
                         result.append(command)
-        
+
         return result
 
     def get_available_commands_str(self) -> List[str]:
@@ -192,9 +199,9 @@ class CommandSystem:
                 for state in self.config.states:
                     if command.name in state.commands:
                         result.append(command.name)
-        
+
         return result
-    
+
     def access_path(
         self,
         input_dict: Union[InputDict, List[str]],
@@ -204,9 +211,9 @@ class CommandSystem:
         """Validates command path by input.
         
         Args:
-            input_dict: A cached input for validation. Accepts `InputDict` for a path and `List[str]` only for a path.
+            input_dict: A parsed input dict. Accepts `InputDict` for a path and `List[str]` only for a path.
             path: A command path, which determines all posible correct ways in path.
-            is_inclusive: Always returns `False` if length of two paths are different. This argument changes validation mode: False (determines any command input from deferred path) and True (determines a single variant for command tree branching).
+            is_inclusive: Always returns `False` if length of two paths are different. This argument changes validation mode: `False` (determines any command input from deferred path) and `True` (determines a single variant for command tree branching).
         
         Returns:
             `False` if input path is mispath. `True` if input path equals the deferred path.
@@ -214,30 +221,30 @@ class CommandSystem:
 
         if isinstance(input_dict, dict):
             input_path: List[str] = input_dict["path"]
-        elif type(input_dict) == list:
+        elif isinstance(input_dict, list):
             input_path: List[str] = input_dict
         else:
             return False
-        
+
         if is_inclusive and len(path) != len(input_path):
             return False
-        
+
         i: int = 0
         for part in path:
             if i >= len(input_path):
                 return True
 
-            if type(part) == str:
-                if input_path[i] == part or part == "-any":
+            if isinstance(part, str):
+                if part in (input_path[i], "-any"):
                     i += 1
                     continue
-            elif type(part) in (list, tuple):
+            elif isinstance(part, (list, tuple)):
                 if input_path[i] in part:
                     i += 1
                     continue
             return False
         return True
-    
+
     def get_command(self, path: CommandPath) -> Optional[CallableCommand]:
         """Get a command from the command routing.
         
@@ -263,10 +270,10 @@ class CommandSystem:
 
         if path in self.route:
             return False
-        
+
         self.route[path] = func
         return True
-    
+
     def change_command(self, path: CommandPath, func: CallableCommand) -> bool:
         """Changes already registered command in the command routing.
         
@@ -280,10 +287,10 @@ class CommandSystem:
 
         if path not in self.route:
             return False
-        
+
         self.route[path] = func
         return True
-    
+
     def unload_command(self, path: CommandPath) -> bool:
         """Removes already registered command from the command routing.
         
@@ -296,10 +303,10 @@ class CommandSystem:
 
         if path not in self.route:
             return False
-        
+
         del self.route[path]
         return True
-    
+
     def get_history(self) -> List[Tuple[str, InputDict]]:
         """Returns all notes from command history.
         
@@ -308,7 +315,7 @@ class CommandSystem:
         """
 
         return self.history["notes"]
-    
+
     def get_history_input(self) -> List[str]:
         """Returns all input strings from command history.
         
@@ -317,7 +324,7 @@ class CommandSystem:
         """
 
         return [note[0] for note in self.history["notes"]]
-    
+
     def get_history_dict(self) -> List[InputDict]:
         """Returns all parsed input dicts from command history.
         
@@ -326,7 +333,7 @@ class CommandSystem:
         """
 
         return [note[1] for note in self.history["notes"]]
-    
+
     def write_history(self, input_str: str, input_dict: InputDict) -> bool:
         """Writes a new note to command history.
         
@@ -338,12 +345,15 @@ class CommandSystem:
             `False` if max note count limit reached and overwrite disabled, else `True`.
         """
 
-        if len(self.history["notes"]) >= self.history["max_size"] and not self.history["is_write_overflow"]:
+        if (
+            len(self.history["notes"]) >= self.history["max_size"]
+            and not self.history["is_write_overflow"]
+        ):
             return False
-        
+
         self.history["notes"].append((input_str, input_dict))
         return True
-    
+
     def read_history(self, index: int) -> Tuple[str, InputDict]:
         """Returns a note by index in saved indexes.
         
@@ -355,8 +365,14 @@ class CommandSystem:
         """
 
         return self.history["notes"][max(min(0, index), self.history["max_size"] - 1)]
-    
-    def execute(self, input_str: str, input_dict: InputDict, normalized_path: List[str], tracking: bool=True) -> bool:
+
+    def execute(
+        self,
+        input_str: str,
+        input_dict: InputDict,
+        normalized_path: List[str],
+        tracking: bool=True
+    ) -> bool:
         """Executes an input string with the command routing.
         
         Args:
@@ -375,11 +391,13 @@ class CommandSystem:
         if len(normalized_path) == 0:
             self.settings.logger.info(Strings.COMMAND_NOT_WRITTEN)
             return False
-        
+
         for path, func in self.route.items():
             if self.access_path(normalized_path, path, False):
                 func(input_str, input_dict)
                 return True
-        
-        self.settings.logger.warning(Strings.COMMAND_NOT_IMPLEMENTED.substitute(input_str=input_str))
+
+        self.settings.logger.warning(
+            Strings.COMMAND_NOT_IMPLEMENTED.substitute(input_str=input_str)
+        )
         return False
