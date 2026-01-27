de191915280607bbefe52f7c4cacc860164a21af
BesBobowyy <banan4ik.stop.channel@gmail.com>
2026-01-27 12:16:26 +0300

    Refactor code as linter rules.

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,9 +1,11 @@
+"""tokenizer.py - Tokenize raw user input string to raw tokens."""
+
+from typing import List, Literal, cast
+
 from src.utils.enums import RawTokenType
 from src.input_token import RawInputToken
 
 from src.settings import Settings
-
-from typing import List, Literal, cast
 
 class Tokenizer:
     """Tokenizer class, which tokenizes user input."""
@@ -30,7 +32,7 @@ class Tokenizer:
         self._input_str = input_str
         self._result = []
         self._i = 0
-    
+
     def tokenize_string(self) -> None:
         """Tokenizes the `String` type."""
 
@@ -44,16 +46,16 @@ class Tokenizer:
                     string += self._input_str[self._i + 1]
                 elif self._input_str[self._i + 1] == '\\':
                     string += self._input_str[self._i]
-                
+
                 self._i += 1
             else:
                 string += self._input_str[self._i]
 
             self._i += 1
-        
+
         self._result.append(RawInputToken(RawTokenType.STRING, string))
         self._settings.logger.debug(f"String: '{string}'")
-    
+
     def tokenize_word(self) -> None:
         """Tokenizes the `Word` type."""
 
@@ -61,7 +63,7 @@ class Tokenizer:
 
         while self._i < len(self._input_str) and self._input_str[self._i].isalnum():
             self._i += 1
-        
+
         word: str = self._input_str[start:self._i]
         self._result.append(RawInputToken(RawTokenType.WORD, word))
         self._settings.logger.debug(f"Word: '{word}'")
@@ -95,12 +97,12 @@ class Tokenizer:
             elif self._input_str[self._i] in ('\'', '\"'):
                 self._settings.logger.debug("Process `String` construction.")
                 self.tokenize_string()
-            
+
             elif self._input_str[self._i].isalnum():
                 self._settings.logger.debug("Process `Word` construction.")
                 self.tokenize_word()
                 continue
-        
+
             self._i += 1
-        
+
         return self._result
